6a9822e7341a6a63b5d2bebb67bdd3ce5c66f08d
BenMan95 <benman9504@gmail.com>
2023-12-17 01:12:59 -0600

    Added icon

--- resources/js/guest.js
+++ resources/js/guest.js
@@ -2,6 +2,6 @@ async function alertAccount() {
     alert('Register or login to interact with posts')
 }
 
-for (let button of document.getElementsByClassName('like')) {
+for (let button of document.getElementsByClassName('button')) {
     button.addEventListener('click', alertAccount)
 }
